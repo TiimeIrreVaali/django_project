ab8cf344f5f8a82f82dec30473f8dedac85ba582
TiimeIrreVaali <mirriy@mail.ru>
2024-10-01 01:10:57 +0500

    Редактирование файла core\views.py

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Приложение core, главная страница')
+    return HttpResponse('Добро пожаловать на главную страницу сайта N!')
 
 def catalog(request):
     return HttpResponse('Приложение core, страница каталога')
